63a779fec71f5a4a87040281e40a1510a6780f56
0tsu0i0ihajime <110276910+0tsu0i0ihajime@users.noreply.github.com>
2023-04-30 13:01:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,26 +8,25 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, "public", "form.html"))
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "form.html"))
+});
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    if(username === 'admin' && password === 'password'){
-        req.session.username = username;
-        res.redirect('/');
+    if(username && password){
+        if(username === 'admin' && password === 'password'){
+            // req.session.username = username;
+            res.redirect('https://www.youtube.com')
+        }else{
+            res.send('/login?error=Invalid username or password')
+        }
     }else{
-        res.redirect('/login?error=Invalid username or password');
-    }
-});
-
-app.get('/', (req, res) => {
-    if(req.session.username === 'admin'){
-        res.render('index', { username: req.session.username });
-    }else{
-        res.render('login');
+        res.send('Please enter a username and password')
+        setTimeout(()=>{
+            res.redirect('https://www.google.com')
+        },2000)
     }
 });
 

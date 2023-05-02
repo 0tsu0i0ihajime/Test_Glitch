7b0d696c29a63032982c99058fc391e55d4be668
0tsu0i0ihajime <110276910+0tsu0i0ihajime@users.noreply.github.com>
2023-05-02 20:17:36 +0900

    Create info.py

---
+++ info.py
@@ -0,0 +1,27 @@
+from yt_dlp import YoutubeDL
+from pprint import pprint
+import sys
+
+url = sys.argv[1:]
+opts = {
+    'ignoreerrors': True,
+    'extract_flat': True
+}
+
+
+def is_playlist(link):
+    links = []
+    with YoutubeDL(opts) as ydl:
+        info = ydl.extract_info(link, download=False)
+        if 'entries' in info:
+            for i in range(len(info['entries'])):
+                links.append(info['entries'][i]['webpage_url'])
+        else:
+            links.append(link)
+    print(links)
+
+if isinstance(url, list):
+    for i in range(len(url)):
+        is_playlist(url[i])
+else:
+    is_playlist(url)
